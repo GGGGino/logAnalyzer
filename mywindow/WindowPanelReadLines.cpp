18a1ef81574c193148dfb01bba2dc2402d7d5674
David Ginanni <david.ginanni@gmail.com>
2019-10-01 09:59:28 +0200

    first try of scroll

--- mywindow/WindowPanelReadLines.cpp
+++ mywindow/WindowPanelReadLines.cpp
@@ -1,34 +1,48 @@
 #include <vector>
+#include <string>
 #include "WindowPanelReadLines.h"
 
 log_analyzer::WindowPanelReadLines::WindowPanelReadLines(int x, int y, int width, int height):
     WindowPanelBase(x, y, width, height) {}
 
 void log_analyzer::WindowPanelReadLines::draw() {
-    int riga = 3;
+    int riga = 3, i = 0, availableHeight = height - 4, cursorScroll = 0;
 
-    auto lineSliced = std::vector<LineParser>(lines_.begin(), lines_.begin() + height - 4);
+    if (selectedLine > availableHeight) {
+        cursorScroll = selectedLine - availableHeight;
+    }
+
+    auto lineSliced = std::vector<LineParser>(lines_.begin() + cursorScroll, lines_.begin() + availableHeight);
 
     for ( LineParser &line: lineSliced ) {
         char cstr[20];
         strcpy(cstr, line.ip_.c_str());
 
-        print_in_body(win, riga, 1, 0, cstr, COLOR_PAIR(0));
+        if (selectedLine == i)
+            print_in_body(win, riga, 1, 0, cstr, COLOR_PAIR(2));
+        else
+            print_in_body(win, riga, 1, 0, cstr, COLOR_PAIR(0));
+
         riga++;
+        i++;
     }
 }
 
 void log_analyzer::WindowPanelReadLines::waitInput(int input) {
     switch(input) {
         case KEY_UP:
-            print_in_middle(win, 3, 3, 20, "Sopra", 2);
+            if (selectedLine > 0) {
+                selectedLine--;
+                draw();
+            }
             break;
         case KEY_DOWN:
-            print_in_middle(win, 3, 3, 20, "Sotto", 2);
+            if (selectedLine < lines_.size()) {
+                selectedLine++;
+                draw();
+            }
             break;
     }
-    update_panels();
-    doupdate();
 }
 
 void log_analyzer::WindowPanelReadLines::addLine(LineParser line) {
